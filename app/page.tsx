53b983a4713b8e3ac0ee8dbd4d0e5475262faa65
Zhila <ministrov2018@gmail.com>
2025-01-04 19:43:45 +0300

    fix - Revome all unneccessery function from home page component to clean

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
 'use client';
 
-import { useState } from 'react';
-
 import ButtonLike from './components/ButtonLike/ButtonLike';
 import BlogCard from './components/BlogCard/BlogCard';
 import { MOCKS } from '@/mocks';
 import styles from './page.module.css';
 
 export default function Home() {
-  const [posts, setPosts] = useState<{ userId: number, id: number, title: string, body: string } | []>([]);
-
   const onPatchRequestHandler = async (id: string) => {
     try {
       const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
@@ -33,23 +29,6 @@ export default function Home() {
     } catch (error) {
       console.log('Error making PATCH request', error);
     }
-    console.log('Successful PATCH request', posts);
-  }
-
-  const getPosts = async () => {
-    try {
-      const response = await fetch('https://dummyjson.com/posts');
-
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-
-      const data = await response.json();
-      setPosts(data["posts"]);
-      console.log(posts);
-    } catch (error) {
-      console.log(error);
-    }
   }
 
   return (
@@ -72,10 +51,6 @@ export default function Home() {
       <br />
 
       <ButtonLike onChange={() => onPatchRequestHandler('1')} />
-
-      <br />
-
-      <ButtonLike onChange={() => getPosts()} />
     </div>
   );
 }
