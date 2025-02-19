5a49e8d5a0cf5c42c2037f48688ed510a458fa93
Zhila <ministrov2018@gmail.com>
2025-02-19 16:51:29 +0300

    feature - Get data from api for all posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,31 @@
-'use client';
-
-import ButtonLike from './components/ButtonLike/ButtonLike';
-import BlogCard from './components/BlogCard/BlogCard';
-import { MOCKS } from '@/mocks';
+import { getPosts } from '@/api/post';
+// import BlogCard from './components/BlogCard/BlogCard';
+// import ButtonLike from './components/ButtonLike/ButtonLike';
+// import BlogCard from './components/BlogCard/BlogCard';
+// import { MOCKS } from '@/mocks';
 import styles from './page.module.css';
 
-export default function Home() {
+export default async function Home() {
+  const posts = await getPosts();
+  const modifiedPosts = Object.values(posts.posts);
 
-
+  console.log(modifiedPosts);
   return (
     <div className={styles.page}>
       <div className={styles.pageCardList}>
-        {MOCKS.map((card) => (
+        {/* {modifiedPosts.map((post) => (
           <BlogCard
-            key={card.id}
+            key={post}
             img={card.img}
             title={card.title}
             text={card.text}
             timeStamp={card.timeStamp}
             likes={card.likes}
           />
-        ))}
+        ))} */}
       </div>
 
-      <ButtonLike />
+      {/* <ButtonLike /> */}
 
       <br />
 
