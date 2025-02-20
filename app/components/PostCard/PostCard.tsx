af094b4100a95a5cf02016437c11cd30ace2b346
Zhila <ministrov2018@gmail.com>
2025-02-20 13:22:47 +0300

    refactore - Change naming of the post card classNames

--- app/components/PostCard/PostCard.tsx
+++ app/components/PostCard/PostCard.tsx
@@ -8,16 +8,16 @@ import styles from './PostCard.module.css';
 const PostCard = ({ title, body, likes, dislikes }: PostCardProps) => {
     return (
         <li className={styles.postCard}>
-            <div className={styles.cardImage}>
+            <div className={styles.postCardImage}>
                 <Image src={'/card-image.png'} width={300} height={192} priority alt={'A placeholder for the image'} />
             </div>
-            <div className={styles.cardMeta}>
-                <ul className={styles.cardMetaList}>
-                    <li className={styles.cardMetaItem}>Front-end</li>
-                    <li className={styles.cardMetaItem}>1 месяц назад</li>
+            <div className={styles.postCardMeta}>
+                <ul className={styles.postCardMetaList}>
+                    <li className={styles.postCardMetaItem}>Front-end</li>
+                    <li className={styles.postCardMetaItem}>1 месяц назад</li>
                 </ul>
 
-                <div className={styles.cardLikes}>
+                <div className={styles.postCardLikes}>
                     <span>{likes}</span>
                     <Image src={'/like.svg'} width={18} height={18} alt='Picture of the thumbs up' />
                 </div>
