52978217204d664da859ab17594d2b2b27d13619
Zhila <ministrov2018@gmail.com>
2025-02-20 13:45:54 +0300

    refactore - Improve a styling for postCard component

--- app/components/PostCard/PostCard.tsx
+++ app/components/PostCard/PostCard.tsx
@@ -11,26 +11,26 @@ const PostCard = ({ title, body, likes, dislikes }: PostCardProps) => {
             <div className={styles.postCardImage}>
                 <Image src={'/card-image.png'} width={300} height={192} priority alt={'A placeholder for the image'} />
             </div>
-            <div className={styles.postCardMeta}>
-                <ul className={styles.postCardMetaList}>
-                    <li className={styles.postCardMetaItem}>Front-end</li>
-                    <li className={styles.postCardMetaItem}>1 месяц назад</li>
-                </ul>
-
-                <div className={styles.postCardLikes}>
-                    <span>{likes}</span>
-                    <Image src={'/like.svg'} width={18} height={18} alt='Picture of the thumbs up' />
-                </div>
-            </div>
             <div className={styles.postCardText}>
+                <div className={styles.postCardMeta}>
+                    <ul className={styles.postCardMetaList}>
+                        <li className={styles.postCardMetaItem}>Front-end</li>
+                        <li className={styles.postCardMetaItem}>1 месяц назад</li>
+                    </ul>
+
+                    <div className={styles.postCardLikes}>
+                        <span>{likes}</span>
+                        <Image src={'/like.svg'} width={18} height={18} alt='Picture of the thumbs up' />
+                    </div>
+                </div>
                 <h3 className={styles.postCardTitle}>{title}</h3>
                 <p className={styles.postCardTextContent}>{body}</p>
+            </div>
 
-                <div className={styles.postCardFooter}>
-                    <span className={styles.postCardTime}>{dislikes} минуты</span>
+            <div className={styles.postCardFooter}>
+                <span className={styles.postCardTime}>{dislikes} минуты</span>
 
-                    <CardLink />
-                </div>
+                <CardLink />
             </div>
         </li>
     )
