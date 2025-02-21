da9766eb93ac68941bb1603edfa8d70a871c9132
Zhila <ministrov2018@gmail.com>
2025-02-21 13:49:28 +0300

    fix - Fixing getting data post page by id out from params props

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,16 +1,19 @@
 import { getPost } from '@/api/postPage';
 import ButtonLike from '@/app/components/ButtonLike/ButtonLike';
 
+import styles from '../../page.module.css';
+
 type PageProps = {
     params: Promise<{ id: string }>
 }
 
 export default async function PostPage({ params }: PageProps) {
-    const page = getPost((await params).id);
+    const page = await getPost((await params).id);
 
-    console.log(page.then(res => res));
+    console.log(page);
     return (
-        <div>
+        <div className={styles.page}>
+            <h2>{page.title}</h2>
             {`Page Post ${(await params).id}`}
             <br />
             {(await params).id}
