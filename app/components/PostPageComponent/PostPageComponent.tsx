e0660e52b00c538b5032a881fca9dd5c5fe43e5c
Zhila <ministrov2018@gmail.com>
2025-03-07 17:37:39 +0300

    feature - Create a layout and markup for post page component

--- app/components/PostPageComponent/PostPageComponent.tsx
+++ app/components/PostPageComponent/PostPageComponent.tsx
@@ -1,14 +1,41 @@
+import Image from 'next/image';
 import React from 'react';
 import { PostPageProps } from './PostPageComponent.props';
 import ButtonLike from '@/app/components/ButtonLike/ButtonLike';
 import styles from './PostPageComponent.module.css';
+import CardLink from '../CardLink/CardLink';
 
 export const PostPageComponent = ({ post }: PostPageProps) => {
     return (
         <div className={styles.postWrapper}>
-            <h2>{post.title}</h2>
+            <h2 className={styles.title}>{post.title}</h2>
 
-            <ButtonLike />
+            <ul className={styles.list}>
+                <li className={styles.item}>Front-end</li>
+                <li className={styles.item}>назад</li>
+                <li className={styles.item}>3 минуты</li>
+                <li className={styles.item}>
+                    4
+                    <CardLink id={'3'} />
+                </li>
+            </ul>
+
+            <Image src={'./post-image.png'} width={687} height={440} alt='Post picture' />
+
+            <p className={styles.desc}>{post.body}</p>
+
+            <h3 className={styles.subtitle}>{post.title}</h3>
+
+            {post.reactions.dislikes && <span className={styles.dislikes}>{post.reactions.dislikes}</span>}
+
+            <p>
+                Грид представляет собой пересекающийся набор горизонтальных и вертикальных линий, образующих колонки и строки. Элементы могут быть помещены в грид в пределах линий этих колонок и строк. Грид имеет следующие особенности:
+            </p>
+
+            <div className={styles.btnBox}>
+                <span>Понравилось? Жми</span>
+                <ButtonLike />
+            </div>
         </div>
     )
 }
