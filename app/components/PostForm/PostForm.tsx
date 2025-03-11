d93db16b388bfaf3a0f475e2ded3ce3755893fef
Zhila <ministrov2018@gmail.com>
2025-03-11 19:33:49 +0300

    feature - Install react hook form library to validate postForm component

--- app/components/PostForm/PostForm.tsx
+++ app/components/PostForm/PostForm.tsx
@@ -1,10 +1,15 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
 import styles from './PostForm.module.css';
 
 export const PostForm = () => {
+    const { register, handleSubmit } = useForm();
     return (
-        <form className={styles.form}>
-            <input className={styles.input} name="forminput" type="text" placeholder="Имя" />
-            <textarea className={styles.textarea} name="textarea" id="" placeholder="Комментарий"></textarea>
+        <form onSubmit={handleSubmit((data) => console.log(data))} className={styles.form}>
+            <input {...register("name")} className={styles.input} name="name" type="text" placeholder="Имя" />
+
+            <textarea {...register("comment")} className={styles.textarea} name="comment" id="" placeholder="Комментарий"></textarea>
 
             <button className={styles.button} type='button'>Отправить</button>
         </form>
