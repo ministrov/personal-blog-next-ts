1e5b43475f4feef00ee2cf77226a5821cab870e9
Zhila <ministrov2018@gmail.com>
2025-03-12 12:36:41 +0300

    feature - Operating on validation post form component

--- app/components/PostForm/PostForm.tsx
+++ app/components/PostForm/PostForm.tsx
@@ -4,7 +4,7 @@ import { useForm } from 'react-hook-form';
 import styles from './PostForm.module.css';
 
 export const PostForm = () => {
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
     const onSubmit = (data: unknown) => {
         console.log(data);
@@ -13,7 +13,11 @@ export const PostForm = () => {
         <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
             <input {...register("name", { required: "Entering name is required", maxLength: 10 })} className={styles.input} name="name" type="text" placeholder="Имя" />
 
+            {errors.name && <p>{errors.root?.message}</p>}
+
             <textarea {...register("comment", { required: "Entering comment is required", maxLength: 150 })} className={styles.textarea} name="comment" id="" placeholder="Комментарий"></textarea>
+
+            {errors.comment && <p>{errors.root?.message}</p>}
 
             <button className={styles.button} type='submit'>Отправить</button>
         </form>
