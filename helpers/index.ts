65a94367f5d6be8e94ac8d18d5805c672cc22427
Zhila <ministrov2018@gmail.com>
2025-03-20 15:04:47 +0300

    feature - Create a memoized function to cach the value

--- helpers/index.ts
+++ helpers/index.ts
@@ -8,10 +8,25 @@ export function truncateText(text: string, maxLength: number):string {
 //     return -1;
 // }
 
-export function incrementValue(value: number | string) {
-    if (typeof value === 'number') {
-        return `${value + 1}`;
-    } else if (typeof value === 'string') {
-        return `${console.log('increament function was invoked')}`;
+export function incrementValue(value: number) {
+    return value + 1;
+};
+
+function memoizeFn(originalFn: (value: number) => void) {
+    const cash = new Map();
+
+    return (value: number) => {
+        if (!cash.has(value)) {
+            cash.set(
+                value,
+                originalFn(value)
+            );
+        }
+        return cash.get(value);
     }
 };
+
+
+const memoizedIncrement = memoizeFn(incrementValue);
+
+console.log(memoizedIncrement);
