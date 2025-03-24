14b14cad3f4eab53a39e3f8a4aea2d237dcdc3a6
Zhila <ministrov2018@gmail.com>
2025-03-24 16:56:03 +0300

    feature - End up operating on creating bannerText component and adding adaptive styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import { getPosts } from "@/api/posts";
 import PostCardList from "./components/PostCardList/PostCardList";
 import { PostItem } from "@/interface/posts.interface";
+import { BannerText } from './components/BannerText/BannerText';
 
 export default async function Home() {
   const posts = await getPosts();
   const modifiedPosts: PostItem[] = Object.values(posts.posts);
 
-  return <PostCardList posts={modifiedPosts} />;
+  return (
+    <section className="main-page">
+      <BannerText content="The Blog"/>
+      <PostCardList posts={modifiedPosts} />
+    </section>
+  );
 }
