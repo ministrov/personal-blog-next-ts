192a06c28ec3b1ce9f185ea6a3c6ec8e3b6302ac
Zhila <ministrov2018@gmail.com>
2025-03-24 11:55:05 +0300

    feature - Create a header navigation

--- layouts/Header/Header.tsx
+++ layouts/Header/Header.tsx
@@ -3,14 +3,28 @@ import Link from 'next/link';
 import Logo from '@/app/components/Logo/Logo';
 import styles from './Header.module.css';
 
+const links = [{ id: 1, name: 'Blog' }, { id: 2, name: 'Projects' }, { id: 3, name: 'About' }, { id: 4, name: 'Newsletter' }];
+
 const Header = () => {
     return (
         <header className={styles.header}>
             <Logo name='Anton Zhilin' />
 
-            <Link href={`#rt`}>
-                <Image className={styles.headerGit} src={'/git-icon.svg'} width={24} height={24} alt='Picture of the GitHub' />
-            </Link>
+            <div className={styles.right}>
+                <nav className={styles.navigation}>
+                    <ul className={styles.list}>
+                        {links.map((link) => (
+                            <li key={link.id} className={styles.item}>
+                                {link.name}
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+
+                <Link href={`#rt`}>
+                    <Image className={styles.headerGit} src={'/git-icon.svg'} width={24} height={24} alt='Picture of the GitHub' />
+                </Link>
+            </div>
         </header>
     )
 }
